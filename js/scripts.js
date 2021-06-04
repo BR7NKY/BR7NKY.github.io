1f6e51436d713b2b197a43a20c48e3285856e980
BR7NKY <m-brinks@live.nl>
2021-06-04 23:28:09 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,10 +33,17 @@
     // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
         target: "#mainNav",
-        
+        offset: 74,
     });
 
-    /
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
     // Collapse now if page is not at top
     navbarCollapse();
     // Collapse the navbar when page is scrolled
